6ea1bc70807c26fa98d55add8da8acd2eab30216
Amrith R Naik <amrithrnaik@gmail.com>
2024-12-25 10:15:22 +0530

    [layout] separated landing page components for desktop and mobile

---
+++ src/lib/components/landing-page.svelte
@@ -0,0 +1,1 @@
+<div class="hidden lg:flex">PC landing page here</div>
---
+++ src/lib/components/mobile-landing-page.svelte
@@ -0,0 +1,28 @@
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+
+	const cloudname = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
+	const videoUrl = `https://res.cloudinary.com/${cloudname}/video/upload/v1735058342/unique-event-productions/banner-video.mp4`;
+</script>
+
+<div class="flex min-h-full w-full flex-col lg:hidden">
+	<!-- Hero Banner Video SECTION -->
+	<video
+		src={videoUrl}
+		aria-label="banner-video"
+		loop
+		muted
+		autoplay
+		class="min-h-[50%] object-cover md:h-[50%]"><track kind="captions" /></video
+	>
+
+	<!-- Texts SECTION -->
+	<div class="flex h-full flex-col items-center justify-center gap-4 p-4 md:h-[40%]">
+		<div class="text-center font-serif text-[1.7rem]">Crafting Unforgettable Experiences</div>
+		<div class="px-2 text-center">
+			At <span class="font-black text-primary">Unique Event Productions</span>, we bring your dreams
+			to life with precision, creativity & passion.
+		</div>
+		<Button variant="secondary">Know More</Button>
+	</div>
+</div>
