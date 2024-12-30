2c41a6bdc09f537644bbe5b3df14d91762f88842
painarasimha <pai.narasimha04@gmail.com>
2024-12-30 21:04:40 +0530

    [layout] our work page layout structured

---
+++ src/routes/our-work/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import OurWork from "$lib/components/our-work/our-work.svelte";
+  import type { WorkItem } from "$lib/types/our-work.ts";
+
+  const workItems: WorkItem[] = [
+    {
+      id:"1",
+      imageUrl: '',
+      category: "Corporate" 
+    },
+    {
+      id:"2",
+      imageUrl: '',
+      category: "Parties" 
+    },
+    {
+      id:"3",
+      imageUrl: '',
+      category: "Corporate" 
+    },
+    {
+      id:"4",
+      imageUrl: '',
+      category: "Concerts" 
+    },
+    {
+      id:"5",
+      imageUrl: '',
+      category: "Corporate" 
+    },
+  ];
+</script>
+
+<OurWork items={workItems} />
