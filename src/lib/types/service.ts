2301fde4ee98221ba710d2c34112aec506c163da
painarasimha <pai.narasimha04@gmail.com>
2024-12-30 14:10:04 +0530

    [refactor] defined type for service card items

---
+++ src/lib/types/service.ts
@@ -0,0 +1,10 @@
+export interface Service {
+  id: string,
+  title: string;
+  shortDesc: string;
+  fullDesc: string;
+  features: string[];
+  imageUrl?: string;
+  info1: string;
+  info2: string;
+}
