e95110cac2af85c787fd1e43471cace74132830e
painarasimha <pai.narasimha04@gmail.com>
2024-12-31 10:01:05 +0530

    [updates] structuring of layout for md breakpoint

--- src/lib/components/services/serviceCard.svelte
+++ src/lib/components/services/serviceCard.svelte
@@ -1,4 +1,12 @@
 <script lang="ts">
+	import img1 from '$lib/assets/wedding.avif';
+	import img2 from '$lib/assets/concerts.avif';
+	import img3 from '$lib/assets/corporate.avif';
+	import img4 from '$lib/assets/rade.avif';
+	import img5 from '$lib/assets/party.avif';
+	import img6 from '$lib/assets/festival.avif';
+	import { onMount } from 'svelte';
+
 	export let service: {
 		id: number;
 		title: string;
@@ -6,23 +14,24 @@
 		imageUrl: string;
 		link: string;
 	};
+
 </script>
 
-<div class="overflow-hidden rounded-lg shadow-lg">
-		<div class="relative h-full">
-			<div class="absolute inset-0 bg-white">
-				<img
-					src={service.imageUrl}
-					alt={service.title}
-					class="h-full w-full object-cover transition-transform duration-300 hover:scale-105"
-				/>
-			</div>
-			<div
-				class="relative flex h-max flex-col justify-end bg-card/40 p-6 transition-colors hover:bg-card/50"
-			>
-				<h3 class="mb-2 text-2xl font-bold text-foreground">{service.title}</h3>
-				<p class="text-foreground/90">{service.shortDesc}</p>
-			</div>
-		</div>
-	</div>
+<div
+  class="overflow-hidden rounded-lg shadow-lg transform transition-transform duration-300 "
+>
+  <div class="relative h-full">
+    <div class="absolute inset-0 bg-white">
+      <img
+        src={img3} 
+        alt={service.title}
+        class="h-full w-full object-cover transition-transform duration-300 hover:scale-105"
+      />
+    </div>
+    <div class="relative flex h-full flex-col justify-end bg-card/40 p-6 transition-colors hover:bg-card/50">
+      <h3 class="mb-2 text-2xl font-bold text-foreground">{service.title}</h3>
+      <p class="text-foreground/90">{service.shortDesc}</p>
+    </div>
+  </div>
+</div>
 
