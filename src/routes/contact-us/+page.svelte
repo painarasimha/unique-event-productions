9bd1fc0f95396bc2f5169f5aa9f70cf2505d1641
painarasimha <pai.narasimha04@gmail.com>
2024-12-31 15:24:54 +0530

    [layout] re-structured layout

--- src/routes/contact-us/+page.svelte
+++ src/routes/contact-us/+page.svelte
@@ -10,12 +10,12 @@
 		{
 			icon: Phone,
 			name: 'Phone',
-			details: ['+1 (555) 123-4567', 'Mon-Fri 9AM-6PM']
+			details: ['']
 		},
 		{
 			icon: Mail,
 			name: 'Email',
-			details: ['contact@eventpro.com', 'support@eventpro.com']
+			details: ['unique.event.productions2020@gmail.com']
 		},
 		{
 			icon: MapPin,
@@ -25,15 +25,15 @@
 	];
 </script>
 
-<div class="container mx-auto px-4 py-6 sm:px-6 sm:py-8 lg:px-8 lg:py-12">
-	<h1 class="mb-8 text-center text-3xl font-bold sm:mb-12 sm:text-4xl lg:text-5xl">Contact Us</h1>
-	<div class="mb-8 grid grid-cols-1 gap-4 sm:mb-12 sm:grid-cols-2 sm:gap-6 lg:grid-cols-3">
+<div class="container mx-auto px-6 py-6 md:px-8 md:py-12">
+	<h1 class="mb-5 text-center text-2xl font-semibold font-serif text-primary md:mb-10 md:font-bold md:text-5xl">Contact Us</h1>
+	<div class="mb-5 grid grid-cols-1 gap-4 sm:mb-12 md:grid-cols-3 sm:gap-6 ">
 		{#each contactInfo as info}
 			<ContactCard {info} />
 		{/each}
 	</div>
 
-  <div class="mb-8 sm:mb-12">
+  <div class="mx-auto mb-8 sm:mb-16 md:w-1/2">
     <Timings />
   </div>
 
