9576752311d83e43edd0d340946c5fdbf6ef0193
painarasimha <pai.narasimha04@gmail.com>
2024-12-31 10:00:15 +0530

    [updates] structuring of layout for desktop

--- src/lib/components/services/desktop-service-grid.svelte
+++ src/lib/components/services/desktop-service-grid.svelte
@@ -12,34 +12,34 @@
 </script>
 
 <div class="container mx-auto px-4 py-6 hidden md:block">
-	<div class="grid grid-cols-12 grid-rows-8 gap-3 items-stretch">
+	<div class="grid grid-cols-6 grid-rows-6 gap-3 items-stretch">
 		<!-- Wedding Planner -->
-		<div class="col-span-8 row-span-4">
+		<div class="col-span-4 row-span-2 items-stretch">
 			<ServiceCard service={services[0]} />
 		</div>
 
 		<!-- Concert & Live Shows -->
-		<div class="col-span-4 row-span-6">
+		<div class="col-span-2 row-span-4 items-stretch">
 			<ServiceCard service={services[1]} />
 		</div>
 
 		<!-- Corporate Events -->
-		<div class="col-span-4 row-span-2">
+		<div class="col-span-2 row-span-2">
 			<ServiceCard service={services[2]} />
 		</div>
 
 		<!-- Exhibition & Trade Shows -->
-		<div class="col-span-4 row-span-2">
+		<div class="col-span-2 row-span-2">
 			<ServiceCard service={services[3]} />
 		</div>
 
 		<!-- Social Parties -->
-		<div class="col-span-6 row-span-2">
+		<div class="col-span-3 row-span-2">
 			<ServiceCard service={services[4]} />
 		</div>
 
 		<!-- Festivals & Cultural Events -->
-		<div class="col-span-6 row-span-2">
+		<div class="col-span-3 row-span-2">
 			<ServiceCard service={services[5]} />
 		</div>
 	</div>
