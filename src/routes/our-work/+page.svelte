53f3f17ca26232d98bd7678828ba8d80dc256d62
painarasimha <pai.narasimha04@gmail.com>
2024-12-31 13:48:44 +0530

    [chore] added links to dummy images

--- src/routes/our-work/+page.svelte
+++ src/routes/our-work/+page.svelte
@@ -1,31 +1,35 @@
 <script lang="ts">
   import OurWork from "$lib/components/our-work/our-work.svelte";
   import type { WorkItem } from "$lib/types/our-work.ts";
+  import concerts from '$lib/assets/concerts.avif';
+  import corporate from '$lib/assets/corporate.avif';
+  import festival from '$lib/assets/festival.avif';
+  import party from '$lib/assets/party.avif';
 
-  const workItems: WorkItem[] = [
+  const workItems: WorkItem[] = [ //TODO Add in proper images
     {
       id:"1",
-      imageUrl: '',
+      imageUrl: `${corporate}`,
       category: "Corporate" 
     },
     {
       id:"2",
-      imageUrl: '',
+      imageUrl: `${party}`,
       category: "Parties" 
     },
     {
       id:"3",
-      imageUrl: '',
-      category: "Corporate" 
-    },
-    {
-      id:"4",
-      imageUrl: '',
+      imageUrl: `${concerts}`,
       category: "Concerts" 
     },
     {
+      id:"4",
+      imageUrl: `${festival}`,
+      category: "Festival" 
+    },
+    {
       id:"5",
-      imageUrl: '',
+      imageUrl: `${corporate}`,
       category: "Corporate" 
     },
   ];
