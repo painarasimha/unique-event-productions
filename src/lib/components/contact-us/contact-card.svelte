333a0ed6e5ff40dbafe3964d6b99646c8efadca2
painarasimha <pai.narasimha04@gmail.com>
2025-01-01 13:32:39 +0530

    [refactor] redirect to call and mail added

--- src/lib/components/contact-us/contact-card.svelte
+++ src/lib/components/contact-us/contact-card.svelte
@@ -11,13 +11,42 @@
 </script>
 
 <div
-	class="flex flex-col items-center rounded-xl shadow-inner shadow-gray-500/20 bg-gray-700/10 border-2 p-4 transition-all hover:shadow-lg sm:p-6"
+	class="flex flex-col items-center rounded-xl border-2 bg-gray-700/10 p-4 shadow-inner shadow-gray-500/20 transition-all hover:shadow-lg sm:p-6"
 >
-	<svelte:component this={info.icon} class="mb-2 h-6 w-6 text-primary text-center md:mb-4 md:h-8 md:w-8" />
-	<h3 class="text-lg font-sans font-medium mb-2 md:text-2xl text-center md:font-semibold">{info.name}</h3>
-  <div class="w-full flex flex-col items-center">
+	<svelte:component
+		this={info.icon}
+		class="mb-2 h-6 w-6 text-center text-primary md:mb-4 md:h-8 md:w-8"
+	/>
+	<h3 class="mb-2 text-center font-sans text-lg font-medium md:text-2xl md:font-semibold">
+		{info.name}
+	</h3>
+	<div class="flex w-full flex-col items-center">
 		{#each info.details as detail}
-			<p class="text-foreground text-[12px] text-center break-words overflow-hidden max-w-full md:text-md md:text-sm">{detail}</p>
+			{#if info.name.toLowerCase().includes('phone')}
+				<a
+					href="tel:{detail.replace(/\D/g, '')}"
+					class="md:text-md max-w-full overflow-hidden break-words text-center text-[12px] text-foreground transition-colors hover:text-primary md:text-sm"
+				>
+					{detail}
+				</a>
+			{:else if info.name.toLowerCase().includes('email') || info.name
+					.toLowerCase()
+					.includes('mail')}
+				<a
+					href="https://mail.google.com/mail/?view=cm&fs=1&to={detail}"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="md:text-md max-w-full overflow-hidden break-words text-center text-[12px] text-foreground transition-colors hover:text-primary md:text-sm"
+				>
+					{detail}
+				</a>
+			{:else}
+				<p
+					class="md:text-md max-w-full overflow-hidden break-words text-center text-[12px] text-foreground md:text-sm"
+				>
+					{detail}
+				</p>
+			{/if}
 		{/each}
 	</div>
 </div>
