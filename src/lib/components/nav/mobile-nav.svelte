c8497b3ed920f33038b072ad1f5654ee204a162a
Amrith R Naik <amrithrnaik@gmail.com>
2025-01-10 09:27:44 +0530

    [refactor] improve mobile nav layout and cursor follower visibility

--- src/lib/components/nav/mobile-nav.svelte
+++ src/lib/components/nav/mobile-nav.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import MobileLink from './mobile-link.svelte';
 	import { Menu } from 'lucide-svelte';
-	import Logo from '$lib/assets/Primary Logo.png'
+	import Logo from '$lib/assets/Primary Logo.png';
 	import * as Sheet from '../ui/sheet/index.ts';
 	import Button from '../ui/button/button.svelte';
 	import ScrollArea from '../ui/scroll-area/scroll-area.svelte';
@@ -12,20 +12,15 @@
 
 <!-- Mobile Nav -->
 <div class="flex w-full items-center justify-between px-4 md:hidden">
-	<img src={Logo} alt="logo-img" class="w-8 h-8">
+	<img src={Logo} alt="logo-img" class="h-8 w-8" />
 
 	<Sheet.Root bind:open>
-		<Sheet.Trigger asChild let:builder>
-			<Button builders={[builder]} variant="ghost" class="p-0">
-				<Menu class="h-7 w-7" />
-				<span class="sr-only">Toggle Menu</span>
-			</Button>
+		<Sheet.Trigger>
+			<Menu class="h-7 w-7 rounded-full border-2 p-1" />
 		</Sheet.Trigger>
 		<Sheet.Content side="right" class="pr-0">
 			<MobileLink href="/" class="flex items-center" bind:open>
-				
 				<img src={Logo} alt="logo-img" class="mr-3 h-7 w-7" />
-				<!-- TODO : Change Website name -->
 				<span class="font-bold">Unique Event Productions</span>
 			</MobileLink>
 			<ScrollArea orientation="both" class="my-4 h-[calc(100vh-8rem)] pb-10 pl-6">
