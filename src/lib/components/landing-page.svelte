e5ecaee81238072323e8bfbb8d6ac87ad9321fe6
painarasimha <pai.narasimha04@gmail.com>
2025-01-11 22:46:13 +0530

    [chore] change the background img and text layout

--- src/lib/components/landing-page.svelte
+++ src/lib/components/landing-page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import { fade, fly, slide } from 'svelte/transition';
 	import { fadeSlideUp } from '$lib/animations.ts';
-	import Button from './ui/button/button.svelte';
 	import { onMount } from 'svelte';
+	import { baseUrl } from '$lib/utils.ts';
+	import Button from './ui/button/button.svelte';
 	import { createEventDispatcher } from 'svelte';
-	import img1 from '$lib/assets/corporate.avif';
-	import img2 from '$lib/assets/festival.avif';
-	import img3 from '$lib/assets/wedding.avif';
+
+	const imgUrl = `${baseUrl}/image/upload/v1736612723/unique-event-productions/banner-img4.jpg`;
 
 	let clients = 0;
 	let events = 0;
@@ -61,63 +61,31 @@
 	});
 </script>
 
-<div class="hidden min-h-full w-full flex-col gap-4 p-4 px-16 lg:flex">
+<div
+	class="hidden min-h-full w-full flex-col gap-4 p-4 px-16 lg:flex"
+	style={`background-image: url('${imgUrl}'); background-size: cover; background-postion: center;`}
+	> <!-- TODO make the image fit correct-->
+	<div class="absolute inset-0 bg-black/20 z-0"></div>
 	<!-- Main SECTION -->
-	<div class="flex h-full w-full gap-8 px-8 py-8">
-		<!-- Left Side Images SECTION -->
-		{#if visible}
-			<div class="flex w-[70%] gap-8">
-				<!-- First Column of 2 Images -->
-				<div class="flex w-full flex-col gap-8">
-					<div class="h-full w-full" in:fadeSlideUp={{ delay: 1 * 200 }}>
-						<img src={img1} alt="Event" class="h-full w-full transform rounded-3xl object-cover shadow-md shadow-white transition-all duration-500 filter blur-[2px] hover:blur-none hover:scale-110" />
-					</div>
-					<div class="h-full w-full" in:fadeSlideUp={{ delay: 2 * 200 }}>
-						<img src={img2} alt="Event" class="h-full w-full transform rounded-3xl object-cover shadow-md shadow-white filter grayscale hover:grayscale-0 transition-all duration-500 hover:scale-110" />
-						<!-- TODO little modification neede - kenBurns-->
-					</div>
-				</div>
-				<!-- Second Column of 1 Image -->
-				<div class="flex w-full items-center">
-					<div class="h-[50%] w-full overflow-hidden group relative rounded-3xl" in:fadeSlideUp={{ delay: 1 * 200 }}>
-						<img src={img3} alt="Event" class="h-full w-full object-cover shadow-md shadow-white" />
-						<div class="absolute inset-0 bg-gradient-to-t from-[#ffb300] to-transparent translate-y-full group-hover:translate-y-20 transition-transform duration-700"></div>
-					</div>
-				</div>
-			</div>
-		{/if}
-
+	<div class="flex h-full w-full gap-8 px-8 py-8 z-10">
 		<!-- Right Side Text SECTION -->
 		<div class="flex w-full flex-col items-center justify-center gap-4">
 			{#if visible}
 				<h1
-					class="animate-gradient-text font-serif text-6xl font-bold text-primary hover:animate-float"
+					class="animate-gradient-text font-serif text-8xl font-bold text-white"
 					in:fade={{ delay: 3 * 300 }}
 				>
 					Unique Event Productions
 				</h1>
-				<p class="text-lg" in:fade={{ delay: 800, duration: 600 }}>Turning Moments into Memories</p>
+				<p class="text-2xl text-white font-medium" in:fade={{ delay: 800, duration: 600 }}>Turning Moments into Memories</p>
 			{/if}
 			<div in:fly={{ y: 20, duration: 500 }}>
 				<Button
 					on:click={handleClick}
-					class="relative overflow-hidden rounded-lg border-2 border-[#ffbf00] bg-transparent px-6 py-2 transition-all duration-300 
-  hover:text-black hover:before:w-full
+					class="relative overflow-hidden rounded-lg bg-[#ffbf00] px-6 py-2 transition-all duration-300 hover:text-black hover:before:w-full
 	{isClicked ? 'scale-95' : 'scale-100'}"
 				>
-					<div
-						class="absolute inset-0 -z-10 w-0 bg-gradient-to-r from-[#ffbf00] to-[#bf9b30] transition-all duration-300"
-					>
-						<span>Learn More</span>
-						{#each particles as particle}
-							<div
-								class="pointer-events-none absolute h-2 w-2 rounded-full bg-[#ffbf00]"
-								style="left: {particle.x}px; top: {particle.y}px; 
-      animation: particle 1s ease-out forwards;"
-							></div>
-							<!-- TODO particles not working -->
-						{/each}
-					</div>
+					<span>Learn More</span>
 				</Button>
 			</div>
 		</div>
@@ -125,11 +93,11 @@
 
 	<!-- Bottom SECTION -->
 	<div
-		class="flex h-fit w-full"
+		class="flex h-fit w-full p-5 z-10"
 		in:slide={{ duration: 1000 }}
 		style="animation: slideIn 0.5s ease-out {3 * 0.2}s forwards;"
 	>
-		<p class="w-full text-center">
+		<p class="w-full text-center text-2xl font-semibold text-white">
 			{clients}+ Happy Clients | {events}+ Events Planned | {experience}+ Years of Experience
 		</p>
 	</div>
