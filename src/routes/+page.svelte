da09896834e21aa844d82785ce657ec4d8bd70ee
Amrith R Naik <amrithrnaik@gmail.com>
2025-01-11 18:08:09 +0530

    [feat] create home page component to consolidate landing page and other sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-	import MobileLandingPage from '$lib/components/mobile-landing-page.svelte';
-	import LandingPage from '$lib/components/landing-page.svelte';
+	import HomePage from '$lib/components/home-page.svelte';
 </script>
 
-<MobileLandingPage />
-<LandingPage />
+<HomePage />
