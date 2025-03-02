82cf27d3581ce4f84447e539150f88a2bb7b7615
painarasimha <pai.narasimha04@gmail.com>
2025-03-02 17:09:24 +0530

    [chore] add shortDesc and image url for service cards

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -2,63 +2,62 @@
 	import DesktopServiceGrid from '$lib/components/services/desktop-service-grid.svelte';
 	import MobileServiceGrid from '$lib/components/services/mobile-service-grid.svelte';
 	import ServiceCard from '$lib/components/services/serviceCard.svelte';
-	const cloudname = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
 	const services = [
 		//TODO Change all the details and information of each services
 		//TODO Fix the Bento Grid Layout of the 2nd Box & use tilt js
 		{
 			id: 1,
 			title: 'Wedding Planner',
-			shortDesc: 'Strategic consulting services for business growth',
-			imageUrl: ``,
+			shortDesc: 'From saat pheras to grand celebrations—crafted with culture, love, and perfection',
+			imageUrl: 'https://res.cloudinary.com/db56favi8/image/upload/v1740907500/unique-event-productions/wedding2.jpg',
 			link: '/service/1',
 			className: 'col-span-2'
 		},
 		{
 			id: 2,
 			title: 'Concerts & Live Shows',
-			shortDesc: 'Digital transformation solutions',
-			imageUrl: ``,
+			shortDesc: 'Beyond the beats, beyond the lights—where energy meets execution',
+			imageUrl: 'https://res.cloudinary.com/db56favi8/image/upload/v1740913810/unique-event-productions/concert1.jpg',
 			link: '/service/2',
 			className: 'col-span-1 row-span-2'
 		},
 		{
 			id: 3,
 			title: 'Corporate Events',
-			shortDesc: 'Custom software development',
-			imageUrl: ``,
+			shortDesc: 'Boardrooms to ballrooms, we script experiences that mean business',
+			imageUrl: 'https://res.cloudinary.com/db56favi8/image/upload/v1740914184/unique-event-productions/coporate1.avif',
 			link: '/service/3',
 			className: 'col-span-1'
 		},
 		{
 			id: 4,
 			title: 'Exhibition & Trade Shows',
-			shortDesc: 'Cloud infrastructure management',
-			imageUrl: ``,
+			shortDesc: 'Your brand, center stage—designed to captivate, built to impress',
+			imageUrl: 'https://res.cloudinary.com/db56favi8/image/upload/v1740907500/unique-event-productions/Exhibition3.jpg',
 			link: '/service/4',
 			className: 'col-span-1'
 		},
 		{
 			id: 5,
 			title: 'Social Parties',
-			shortDesc: 'Data analytics and insights',
-			imageUrl: ``,
+			shortDesc: 'Ordinary gatherings? Not on our watch—expect the unexpected',
+			imageUrl: 'https://res.cloudinary.com/db56favi8/image/upload/v1740907500/unique-event-productions/Party1.jpg',
 			link: '/service/5',
 			className: 'col-span-1'
 		},
 		{
 			id: 6,
 			title: 'Festival & Cultural Events',
-			shortDesc: 'Data analytics and insights',
-			imageUrl: ``,
+			shortDesc: 'Tradition meets transformation—celebrations redefined',
+			imageUrl: 'https://res.cloudinary.com/db56favi8/image/upload/v1740914903/unique-event-productions/festival1.jpg',
 			link: '/service/6',
 			className: 'col-span-2'
 		}
 	];
 </script>
 
-<div class="mx-auto flex max-w-screen-xl bg-gray-200/70 border flex-col py-6 md:container">
-	<h1 class="mx-auto mb-8 text-center font-serif text-2xl md:text-5xl">Services</h1>
+<div class="flex flex-col bg-gray-200/70 mx-auto py-6 border max-w-screen-xl md:container">
+	<h1 class="mx-auto mb-8 font-serif text-2xl md:text-5xl text-center">Services</h1>
 	<MobileServiceGrid {services} />
 	<DesktopServiceGrid {services} />
 </div>
