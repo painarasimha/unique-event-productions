243a76a2ed81628b2cd43be143841c52d9130f0b
painarasimha <pai.narasimha04@gmail.com>
2025-03-03 10:01:02 +0530

    [chore] timing info updated

--- src/lib/components/contact-us/timings.svelte
+++ src/lib/components/contact-us/timings.svelte
@@ -6,17 +6,17 @@
 
   const businessHours: Schedule[] = [
     { days: "Monday - Friday", hours: "9:00 AM - 6:00 PM"},
-    { days: "Saturday", hours: "10:00 AM - 4:00 PM"},
-    { days: "Sunday", hours: "Closed"}
+    { days: "Saturday", hours: "9:00 AM - 4:00 PM"},
+    { days: "Sunday", hours: "10:00 AM - 12:00 PM"}
   ];
 </script>
 
-<div class="bg-gray-700/10 rounded-xl shadow-inner mb-4 shadow-gray-500/20 p-4 border-2 sm:p-6 lg:p-8">
-  <h3 class="text-lg font-sans font-medium mb-4 text-center md:font-semibold md:text-2xl">Business Hours</h3>
+<div class="bg-gray-700/10 shadow-gray-500/20 shadow-inner mb-4 p-4 sm:p-6 lg:p-8 border-2 rounded-xl">
+  <h3 class="mb-4 font-sans font-medium md:font-semibold text-lg md:text-2xl text-center">Business Hours</h3>
   <div class="space-y-2 md:space-y-5">
     {#each businessHours as schedule}
-      <div class="flex flex-col items-center ">
-        <span class="text-foreground font-medium text-sm md:text-lg">{schedule.days}</span>
+      <div class="flex flex-col items-center">
+        <span class="font-medium text-foreground text-sm md:text-lg">{schedule.days}</span>
         <span class="font-medium text-sm md:text-base">{schedule.hours}</span>
       </div>
     {/each}
