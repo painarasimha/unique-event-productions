d3377164d1b948e5c2b8e077aaa6cdfde5c92ee3
painarasimha <pai.narasimha04@gmail.com>
2025-03-03 10:01:26 +0530

    [chore] add FAQs

--- src/lib/components/contact-us/faq-section.svelte
+++ src/lib/components/contact-us/faq-section.svelte
@@ -8,35 +8,39 @@
 
 	const faqs: FAQItem[] = [
 		{
-			question: 'Do you handle international events?',
-			answer: 'Yes, we organize events worldwide with our network of global partners.'
+			question: 'Do you offer end-to-end event management?',
+			answer: 'Absolutely! From venue selection, décor, and catering to artist bookings and logistics, we handle everything.'
 		},
 		{
 			question: "What's your typical event planning timeline?",
-			answer: 'We recommend 3-6 months for medium events and 6-12 months for large events.'
+			answer: 'We recommend atleast 1 month in advance for small or medium events and 2-3 months for large events.'
+		},
+		{
+			question: 'Do you provide vendor coordination?',
+			answer: 'Yes,  we have tie-ups with top photographers, caterers, decorators, and makeup artists to ensure seamless execution of all event needs.'
+		},
+		{
+			question: 'How can I reach you for urgent queries?',
+			answer: 'You can call or WhatsApp us at +91 99026 64066  or email us at'
 		},
 		{
 			question: "What's your cancellation policy?",
 			answer: 'Full refund if cancelled 30 days before the event, 50% refund within 15-29 days.'
 		},
-		{
-			question: 'Do you provide vendor recommendations?',
-			answer: 'Yes, we have a curated list of trusted vendors for all event needs.'
-		}
 	];
 </script>
 
 <div
-	class="mx-auto w-full max-w-3xl rounded-xl border-2 bg-gray-700/10 px-4 py-10 shadow-inner shadow-gray-500/20 md:px-8"
+	class="bg-gray-700/10 shadow-gray-500/20 shadow-inner mx-auto px-4 md:px-8 py-10 border-2 rounded-xl w-full max-w-3xl"
 >
-	<h2 class="mb-4 text-center text-lg font-medium md:mb-8 md:text-2xl md:font-bold">
+	<h2 class="mb-4 md:mb-8 font-medium md:font-bold text-lg md:text-2xl text-center">
 		Frequently Asked Questions
 	</h2>
 	<Accordion.Root>
 		{#each faqs as faq, i}
 			<Accordion.Item value={`item-${i}`} class="text-sm md:text-lg">
 				<Accordion.Trigger class="py-3 md:py-5">{faq.question}</Accordion.Trigger>
-				<Accordion.Content class="px-4 py-3 text-start text-foreground md:py-5">{faq.answer}</Accordion.Content>
+				<Accordion.Content class="px-4 py-3 md:py-5 text-foreground text-start">{faq.answer}</Accordion.Content>
 			</Accordion.Item>
 		{/each}
 	</Accordion.Root>
